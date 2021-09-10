1d4760682c2a4911f72749a64d6642052b7ff02b
Deep <deeppandya91@gmail.com>
2021-09-10 17:08:42 -0400

    Remove apps section from MVP

--- android/java/org/chromium/chrome/browser/crypto_wallet/adapters/CryptoFragmentPageAdapter.java
+++ android/java/org/chromium/chrome/browser/crypto_wallet/adapters/CryptoFragmentPageAdapter.java
@@ -5,12 +5,16 @@
 
 package org.chromium.chrome.browser.crypto_wallet.adapters;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import org.chromium.base.ContextUtils;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.crypto_wallet.fragments.AccountsFragment;
 import org.chromium.chrome.browser.crypto_wallet.fragments.PortfolioFragment;
 
@@ -19,8 +23,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CryptoFragmentPageAdapter extends FragmentStatePagerAdapter {
+    private Context context = ContextUtils.getApplicationContext();
     private final List<String> titles =
-            new ArrayList<>(Arrays.asList("Portfolio", "Apps", "Accounts"));
+            new ArrayList<>(Arrays.asList(context.getResources().getString(R.string.portfolio),
+                    context.getResources().getString(R.string.accounts)));
     public CryptoFragmentPageAdapter(FragmentManager fm) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
     }
