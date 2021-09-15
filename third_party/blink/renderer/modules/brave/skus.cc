c4b5f011d33651acb028908a093378515ceb11b3
Brian Clifton <brian@clifton.me>
2021-09-15 01:40:25 -0700

    rev1

--- third_party/blink/renderer/modules/brave/skus.cc
+++ third_party/blink/renderer/modules/brave/skus.cc
@@ -1,33 +1,33 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/third_party/blink/renderer/modules/brave/brave.h"
-
 #include "brave/third_party/blink/renderer/modules/brave/skus.h"
+
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 
 namespace blink {
 
-ScriptPromise Brave::isBrave(ScriptState* script_state) {
+ScriptPromise Skus::refresh_order(ScriptState* script_state) {
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
+
+  // TODO: do the magic here
   resolver->Resolve(true);
+
   return promise;
 }
 
-Skus* Brave::skus() {
-  if (!skus_) {
-    skus_ = MakeGarbageCollected<Skus>();
-  }
-  return skus_;
-}
+ScriptPromise Skus::fetch_order_credentials(ScriptState* script_state) {
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+  ScriptPromise promise = resolver->Promise();
 
-void Brave::Trace(blink::Visitor* visitor) const {
-  visitor->Trace(skus_);
-  ScriptWrappable::Trace(visitor);
+  // TODO: do the magic here
+  resolver->Resolve(true);
+
+  return promise;
 }
 
 }  // namespace blink
